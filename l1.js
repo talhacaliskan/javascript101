9891c9bd5167d18d35c88bca0de8652cc62eb0f5
Talha ÇALIŞKAN <talhacaliskan@Talhas-MacBook-Pro.local>
2018-04-20 16:24:54 +0300

    ders 1 tamamlandı

---
+++ l1.js
@@ -0,0 +1,64 @@
+//var variable
+//boolen 0 ise false , 1 ise true
+//DEĞİŞKENLER
+var sayi1 = 10;
+var sayi2 = 5;
+//console.log(sayi1);
+
+var metin1 = "Burak KIRALDIR !!!!11";
+var dogrumusamet = false;
+var kontrol;
+kontrol = "faruk demirtaş";
+//console.log(metin1);
+
+//OPERATORLER
+//+ - / * 
+//+ + 
+
+//== && || > < >= <= !=
+//console.log(sayi1+sayi2);
+
+//IF - ELSE
+// == , === eşit ve tipleri aynımı ,
+//7 sayi1 = 5 metin = "5"
+// != sayi1 != sayi2 
+// >= , > , < , <=
+
+
+// if(sayi1 == sayi2){
+//  console.log("sayi1 ve sayi2 eşittir");
+// }
+// else {
+//     console.log("sayi 1 ve sayi2 eşit değildir");
+// }
+
+// else if (sayi1 === sayi2){
+//   console.log("sayi1 ve sayi2 eşittir");
+// }
+// else {
+//     console.log("sayi1 ve sayi2 eşit değildir");
+// }
+//console.log(kontrol);
+
+//SWITCH - CASE 
+var sayi = 16;
+switch(sayi){
+    case 1:
+        console.log("Sayımız 1");
+        break;
+    case 2:
+    console.log("Sayımız 2");
+        break;
+    case 3:
+    console.log("Sayımız 3");
+        break;
+    case 4:
+    console.log("Sayımız 4");
+        break;
+    case 5:
+    console.log("Sayımız 5");
+        break;
+    default:
+    console.log("sen gelme ulan ayı!");
+        break;
+}
